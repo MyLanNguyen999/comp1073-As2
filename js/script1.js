4e18138dd8c53d84e3b8223861e3182914c5a8af
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-03-18 09:05:28 -0400

    add firstName and lastName fields to form

--- js/script1.js
+++ js/script1.js
@@ -1,6 +1,8 @@
 // * Define the Smoothie class
 class Smoothie {
-    constructor(size, includeMilk, fruitChoice) {
+    constructor(firstName, lastName, size, includeMilk, fruitChoice) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.size = size;
         this.includeMilk = includeMilk;
         this.fruitChoice = fruitChoice;
@@ -9,6 +11,7 @@ class Smoothie {
     smoothieDesc() {
         output.innerHTML = `Your smoothie will be:
         <ul>
+            <li> Order Name: ${this.firstName} ${this.lastName} </li>
             <li> Size: ${this.size} </li>
             <li> Milk: ${this.includeMilk} </li>
             <li> Fruit: ${this.fruitChoice} </li>
@@ -20,18 +23,20 @@ class Smoothie {
 function orderSmoothie(event) {
     event.preventDefault();
     // Grab the current values of the form elements
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value; 
     let size = document.getElementById("size").value;
     let includeMilk = document.getElementById("includeMilk").value;
     let fruitChoice = document.getElementById("fruitChoice").value;
 
     // * Check if any required field is empty
-        if (size === "" || includeMilk === "" || fruitChoice === "") {
+        if (firstName === "" || lastName === "" || size === "" || includeMilk === "" || fruitChoice === "") {
             // alert("Please select options for all fields.");
             output.textContent = `Please select options for all fields.`;
         } else {
             // * If all required fields are filled out, proceed with the review
             // * Create a Smoothie object with the user's selections
-            let mySmoothie = new Smoothie(size, includeMilk, fruitChoice);
+            let mySmoothie = new Smoothie(firstName, lastName, size, includeMilk, fruitChoice);
 
             // * Call the smoothieDesc method to display the smoothie description
             mySmoothie.smoothieDesc();
@@ -41,8 +46,3 @@ function orderSmoothie(event) {
 // * Add event listener to the Order button to trigger the orderSmoothie function
 document.getElementById("reviewButton").addEventListener("click", orderSmoothie);
 
-// *
-const orderConfirm = document.getElementById("orderConfirm");
-let firstName = document.getElementById("firstName");
-let lastName = document.getElementById("lastName");
-orderConfirm.textContent = `Thank you for choosing us, ${firstName} ${lastName}`;
