c9c6e2ade097d90fbd6ecc0e91fa5a2ac3b94e0d
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-03-18 08:53:23 -0400

    add fields to index page

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,4 @@
-// Define the Smoothie class
+// * Define the Smoothie class
 class Smoothie {
     constructor(size, includeMilk, fruitChoice) {
         this.size = size;
@@ -16,7 +16,7 @@ class Smoothie {
     }
 }
 
-// Function to handle form submission
+// * Function to handle form submission
 function orderSmoothie(event) {
     event.preventDefault();
     // Grab the current values of the form elements
@@ -24,19 +24,25 @@ function orderSmoothie(event) {
     let includeMilk = document.getElementById("includeMilk").value;
     let fruitChoice = document.getElementById("fruitChoice").value;
 
-    // Check if any required field is empty
+    // * Check if any required field is empty
         if (size === "" || includeMilk === "" || fruitChoice === "") {
             // alert("Please select options for all fields.");
             output.textContent = `Please select options for all fields.`;
         } else {
-            // If all required fields are filled out, proceed with the review
-            // Create a Smoothie object with the user's selections
+            // * If all required fields are filled out, proceed with the review
+            // * Create a Smoothie object with the user's selections
             let mySmoothie = new Smoothie(size, includeMilk, fruitChoice);
 
-            // Call the smoothieDesc method to display the smoothie description
+            // * Call the smoothieDesc method to display the smoothie description
             mySmoothie.smoothieDesc();
                 }
 }
 
-// Add event listener to the Order button to trigger the orderSmoothie function
+// * Add event listener to the Order button to trigger the orderSmoothie function
 document.getElementById("reviewButton").addEventListener("click", orderSmoothie);
+
+// *
+const orderConfirm = document.getElementById("orderConfirm");
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+orderConfirm.textContent = `Thank you for choosing us, ${firstName} ${lastName}`;
