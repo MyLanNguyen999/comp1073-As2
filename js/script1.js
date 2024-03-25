8f7e10e8968d2a158bae63add6e76dec5d283e46
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-03-25 13:39:28 -0400

    add JS for smoothie image ad update CSS

--- js/script1.js
+++ js/script1.js
@@ -1,3 +1,8 @@
+// * grab output id from html
+const output = document.querySelector('#output');
+// * grab the aside element from html
+const aside = document.querySelector("aside");
+
 // * Define the Smoothie class
 class Smoothie {
     constructor(firstName, lastName, size, includeMilk, fruitChoice) {
@@ -19,10 +24,13 @@ class Smoothie {
     }
 }
 
+// * create element for image
+let image = document.createElement("img");
+
 // * Function to handle form submission
 function orderSmoothie(event) {
     event.preventDefault();
-    // Grab the current values of the form elements
+    // * Grab the current values of the form elements
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value; 
     let size = document.getElementById("size").value;
@@ -30,13 +38,40 @@ function orderSmoothie(event) {
     let fruitChoice = document.getElementById("fruitChoice").value;
 
     // * Check if any required field is empty
-        if (firstName === "" || lastName === "" || size === "" || includeMilk === "" || fruitChoice === "") {
-            // alert("Please select options for all fields.");
+        if (firstName === "" || 
+            lastName === "" || 
+            size === "" || 
+            includeMilk === "" || 
+            fruitChoice === "") {
+
+            // * alert("Please select options for all fields.");
             output.textContent = `Please select options for all fields.`;
+
         } else {
+
             // * If all required fields are filled out, proceed with the review
             // * Create a Smoothie object with the user's selections
             let mySmoothie = new Smoothie(firstName, lastName, size, includeMilk, fruitChoice);
+
+            // * check the fruit choice and show smoothie image accordingly
+            if ( fruitChoice == "Mango") {
+                image.setAttribute("src", "images/mango.png");
+            } 
+            else if ( fruitChoice == "Strawberry") {
+                image.setAttribute("src", "images/strawberry.png");
+            }
+            else if ( fruitChoice == "Orange") {
+                image.setAttribute("src", "images/orange.png");
+            } 
+            else if ( fruitChoice == "Vanilla") {
+                image.setAttribute("src", "images/vanilla.png");
+            } 
+            else if ( fruitChoice == "Chocolate") {
+                image.setAttribute("src", "images/chocolate.png");
+            }  
+
+            // * append the image to the aside element
+                aside.appendChild(image);
 
             // * Call the smoothieDesc method to display the smoothie description
             mySmoothie.smoothieDesc();
@@ -46,3 +81,5 @@ function orderSmoothie(event) {
 // * Add event listener to the Order button to trigger the orderSmoothie function
 document.getElementById("reviewButton").addEventListener("click", orderSmoothie);
 
+// * Grab the <aside> element from html for showing the image
+// const smoothieImage = document.querySelector("aside");
